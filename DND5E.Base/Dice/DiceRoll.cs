f1525d11df7ee53a012a783153e053beae6ab59b
William McGraw <billmcg90@gmail.com>
2018-10-21 11:47:14 -0400

    Add project files.

---
+++ DND5E.Base/Dice/DiceRoll.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DND5E.Base.Dice
+{
+	public abstract class DiceRoll
+	{
+		protected static Random random = new Random();
+		public abstract int Roll();
+	}
+}
---
+++ DND5E.Base/Dice/SimpleRoll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DND5E.Base.Dice
+{
+	public class SimpleRoll : DiceRoll
+	{
+		private int _nDice;
+		private int _nSides;
+		private int _constant;
+
+		public SimpleRoll(int nDice, int nSides, int constant)
+		{
+			_nDice = nDice;
+			_nSides = nSides;
+			_constant = constant;
+		}
+
+		public static int RollDice(int nDice, int nSides, int constant)
+		{
+			return Enumerable
+				.Range(1, nDice)
+				.Select(i => random.Next(1, nSides))
+				.Sum() + constant;
+		}
+
+		public override int Roll()
+		{
+			return SimpleRoll.RollDice(_nDice, _nSides, _constant);
+		}
+	}
+}
